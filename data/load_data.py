d99969428db4d97b585fe92dd8f36dea8f67617a
henry <henrykryon@gmail.com>
2024-02-07 17:42:53 -0300

    changed to lightning

--- data/load_data.py
+++ data/load_data.py
@@ -1,6 +1,9 @@
 import torchvision.datasets as datasets
 from torch.utils import data
 import torchvision.transforms as transforms
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class Data:
@@ -9,26 +12,30 @@ class Data:
         self.mnist_train = datasets.MNIST(
             "data",
             train=True,
-            download=False,
+            download=True,
             transform=transforms.Compose(
                 [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))],
             ),
         )
         self.mnist_test = datasets.MNIST(
             "data",
-            train=False,
+            train=True,
             download=False,
             transform=transforms.Compose(
                 [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))],
             ),
         )
 
-    def get_dataloaders(self, train_bs=512, test_bs=128):
+    def get_dataloaders(self, train_bs=30000, test_bs=1024):
         train_loader = data.DataLoader(
-            self.mnist_train, batch_size=train_bs, shuffle=True
+            self.mnist_train,
+            batch_size=train_bs,
+            shuffle=True,
         )
         test_loader = data.DataLoader(
-            self.mnist_test, batch_size=test_bs, shuffle=False
+            self.mnist_test,
+            batch_size=test_bs,
+            shuffle=False,
         )
         return train_loader, test_loader
 
--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import torch
 from data.load_data import Data
 from model.model import VAE
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 data = Data()
 train_loader, test_loader = data.get_dataloaders()
 data.show_images(train_loader)
-model = VAE(hidden_dim=2)
+model = VAE(hidden_dim=2).to(device)
 
 
 loss_fn = torch.nn.MSELoss()
@@ -16,8 +17,9 @@ def train(num_epochs):
     for epoch in range(num_epochs):
         model.train()
         total_loss = 0
-        for _, (inputs, targets) in enumerate(train_loader):
+        for _, (inputs, _) in enumerate(train_loader):
             optimizer.zero_grad()
+            inputs = inputs.to(device)
             _, outputs, _, _ = model(inputs)
             loss = loss_fn(outputs, torch.nn.Flatten()(inputs))  # + kl_inputs)
             loss.backward()
@@ -25,15 +27,16 @@ def train(num_epochs):
             total_loss += loss.item()
         print(f"Epoch {epoch+1}, Loss: {total_loss/len(train_loader)}")
 
-    # Evaluate on validation set after each epoch
-    model.eval()
-    val_loss = 0
-    with torch.no_grad():
-        for inputs, targets in enumerate(test_loader):
-            outputs = model(inputs)
-            loss = loss_fn(outputs, targets)
-            val_loss += loss.item()
-        print("val_loss", val_loss / len(test_loader))
+        # Evaluate on validation set after each epoch
+        model.eval()
+        val_loss = 0
+        with torch.no_grad():
+            for _, (inputs, _) in enumerate(test_loader):
+                inputs = inputs.to(device)
+                _, outputs, _, _ = model(inputs)
+                loss = loss_fn(outputs, torch.nn.Flatten()(inputs))
+                val_loss += loss.item()
+            print("val_loss", val_loss / len(test_loader))
 
 
 train(num_epochs=10)
